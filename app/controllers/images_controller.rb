53f77703f15ab9de41483162d6156bc26e432734
manemone <hamada.kazuki.mum@gmail.com>
2015-10-30 01:55:21 +0900

    add image uploading feature

---
+++ app/controllers/images_controller.rb
@@ -0,0 +1,28 @@
+class ImagesController < ApplicationController
+  def new
+    @image = Image.new
+  end
+  
+  def create
+    begin
+      @image = Image.create!(image_params)
+      redirect_to image_url(@image)
+    rescue
+      redirect_to :new
+    end
+  end
+
+  def show
+    begin
+      @image = Image.find(params[:id])
+    rescue
+      redirect_to :new
+    end
+  end
+
+  private
+
+  def image_params
+    params.require(:image).permit(:image, :image_cache)
+  end
+end
