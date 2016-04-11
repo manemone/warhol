9e09a0730c2281cffa2de524daf904715872fec0
manemone <hamada.kazuki.mum@gmail.com>
2016-04-12 03:04:59 +0900

    implement github login

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,8 @@ Rails.application.routes.draw do
   root 'images#new'
 
   resources :images, only: [:new, :create, :show]
+
+  # user authentication
+  get 'auth/:provider/callback', to: 'sessions#callback'
+  get '/logout', to: 'sessions#destroy'
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151026162341) do
+ActiveRecord::Schema.define(version: 20160411172238) do
 
   create_table "images", force: :cascade do |t|
     t.string   "name"
@@ -21,6 +21,15 @@ ActiveRecord::Schema.define(version: 20151026162341) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string   "image"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "name"
+    t.string   "provider"
+    t.text     "uid"
+    t.string   "oauth_token"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
   end
 
 end
