f739a3bc194300d7fa7a4990f7b59622606d0dd6
manemone <hamada.kazuki.mum@gmail.com>
2016-04-15 02:55:17 +0900

    set user model to generate its first api_key on creation

--- app/models/api_key.rb
+++ app/models/api_key.rb
@@ -1,4 +1,26 @@
 class ApiKey < ActiveRecord::Base
   belongs_to :user
+
+  before_create :generate_access_token
+  before_create :set_expiration
+  before_create :set_active
+
+  def before_expired?
+    DateTime.now < self.expires_at
+  end
+
+  def set_active
+    self.active = true
+  end
+
+  def set_expiration
+    self.expires_at = Time.now.years_since(50)
+  end
+
+  private
+
+  def generate_access_token
+    self.access_token = SecureRandom.hex
+  end
 end
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ActiveRecord::Base
   has_many :api_keys
 
+  before_create :ensure_to_have_at_least_one_api_key
+
   def self.create_with_omniauth(auth)
     create! do |user|
       user.provider = auth['provider']
@@ -8,5 +10,11 @@ class User < ActiveRecord::Base
       user.name = auth['info']['nickname']
       user.oauth_token = auth['credentials']['token']
     end
+  end
+
+  private
+
+  def ensure_to_have_at_least_one_api_key
+    api_keys.build if api_keys.blank?
   end
 end
