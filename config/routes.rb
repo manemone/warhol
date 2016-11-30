8b6833ac1153f80539254c314018de2d199ce121
manemone <hamada.kazuki.mum@gmail.com>
2016-12-01 00:50:28 +0900

    Define route to resize image

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
   root 'sessions#new'
 
-  resources :images, only: [:index, :create, :show, :destroy]
+  resources :images, only: [:index, :create, :show, :destroy] do
+    member do
+      get ':geometry', constraints: { geometry: /(\A\d+x\d+\Z)|(\Ax\d+\Z)|(\A\d+x\Z)/ }, to: "images#resize"
+    end
+  end
   resource :user, only: [:show] do
     resource :api_key, only: [:update]
   end
